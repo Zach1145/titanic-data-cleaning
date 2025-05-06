f301a76b5b7c0638c6e4148c98b383c0bb0ec4a1
Zach1145 <z.bower94@gmail.com>
2025-05-05 20:39:30 -0400

    Add data cleaning script and cleaned dataset

---
+++ clean_titanic.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2025 [Zachary Bower]
+# Licensed under the MIT License. See LICENSE file in the repository root.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sqlite3
+from sqlite3 import Error
+
+# Read in the csv file
+df = pd.read_csv('train.csv')
+
+# Drop any duplicate rows
+df.drop_duplicates(inplace=True)
+
+# Drop unneeded columns
+df.drop(['Name', 'Ticket', 'Cabin', 'Fare'], axis=1, inplace=True)
+
+# Fill in missing ages with the mean of all ages
+df.fillna({'Age': round(df['Age'].mean())}, inplace=True)
+
+# Fill in missing embarked locations with the most common one, Standardize input capitalization
+df.fillna({'Embarked': df['Embarked'].mode()[0]}, inplace=True)
+df['Embarked'] = df['Embarked'].str.lower()
+
+# Save cleaned data to csv
+df.to_csv('titanic_cleaned.csv', index=False)
